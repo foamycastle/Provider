d5ea67c35a2067f6d963d47c74f843ee9d9937e1
Aaron Sollman <unclepong@gmail.com>
2025-10-21 16:15:05 -0500

    added NumberProvider.php interface implementation

--- src/Provider/NumberProvider.php
+++ src/Provider/NumberProvider.php
@@ -6,4 +6,48 @@ use FoamyCastle\Provider\Provider;
 
 abstract class NumberProvider extends Provider implements NumberProviderContract
 {
+    /**
+     * @inheritDoc
+     */
+    function shiftLeft(int $byBits): \FoamyCastle\Provider\NumberProviderContract
+    {
+        $this->data >>= $byBits;
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    function shiftRight(int $byBits): \FoamyCastle\Provider\NumberProviderContract
+    {
+        $this->data <<= $byBits;
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    function andOp(int $andValue): \FoamyCastle\Provider\NumberProviderContract
+    {
+        $this->data &= $andValue;
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    function orOp(int $orValue): \FoamyCastle\Provider\NumberProviderContract
+    {
+        $this->data |= $orValue;
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    function invert(): \FoamyCastle\Provider\NumberProviderContract
+    {
+        $this->data = ~$this->data;
+        return $this;
+    }
 }
