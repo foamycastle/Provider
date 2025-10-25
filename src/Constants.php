4db122afda82a5a1d995df2755d57a21989a328a
Aaron Sollman <unclepong@gmail.com>
2025-10-25 12:41:09 -0500

    update terminal commands

--- src/Constants.php
+++ src/Constants.php
@@ -1,13 +1,26 @@
 <?php
 
+/**
+ * Commands used for retrieving the hardware mac address of a system, required for certain versions of UUID
+ */
 const NETWORK_ADAPTER_COMMANDS=[
+    //Linux Commands
     'LIN'=>[
 
         ['ifconfig -a', '/(?i)(?<=ether |hwaddr )([a-f0-9(\-:)]{17})/'],
         ['ip -a',       '/(?i)(?<=ether |hwaddr )([a-f0-9(\-:)]{17})/']
     ],
+    //Windows commands
     'WIN'=>[
         ['ipconfig /all | findstr "Physical Address"']
+    ],
+    //Mac OS Commands
+    'DAR'=>[
+        ['ifconfig',    '/(?i)(?<=ether |hwaddr )([a-f0-9(\-:)]{17})/']
+    ],
+    //FREE BSD commands
+    'FRE'=>[
+        ['ifconfig',    '/(?i)(?<=ether |hwaddr )([a-f0-9(\-:)]{17})/']
     ]
 
 ];
